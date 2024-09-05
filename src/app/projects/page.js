25422c1b9cd996a9d593fb67124b3b64412da195
Thang Le - INDX <tle@intelligentdx.com>
2024-09-05 17:32:08 -0400

    removed professional experience

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -70,58 +70,6 @@ export default function Home() {
           </div>
         </div>
       </Layout>
-
-      <Layout>
-        <div className="flex flex-col items-center justify-center w-full py-12">
-          <p className="text-center text-6xl uppercase">Professional Projects</p>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8"> {/* 2x2 Grid container */}
-            {[{
-                title: "Robust Messaging System",
-                description: "A microservice that allows clinics to notify patients via SMS or email",
-                role: "Backend Developer",
-                language: "Typescript",
-                link: "https://docs.google.com/presentation/d/1JxtlVux2_AiJC3rpf-oy-X4p-TFVShbbiXWnRXvo2q4/edit?usp=sharing"
-              },
-              {
-                title: "Patient Facing Application",
-                description: "PWA that allows patients to manage appointments, copay, and documents",
-                language: "Golang, React, JavaScript",
-                role: "Lead Backend Developer, Frontend Developer, Scrum Master, Tech Lead",
-                link: "https://github.com/Thangle8501340/Coding-Questions"
-              },
-              {
-                title: "Automated Chatbot",
-                description: "An automated AI chatbot to help patients manage their appointments.",
-                language: "Golang",
-                role:"Lead Backend Developer, Tech Lead",
-                link: "https://github.com/Thangle8501340/Thangle8501340/tree/main/thang-portfolio"
-              },
-              {
-                title: "Medical Billing Rules Engine", 
-                description: "A medical billing rules engine to verify patients' eligibility for insurance plans. Role: Backend Developer",
-                role: "Backend Developer",
-                language: "Python",
-        
-                link: "https://github.com/Frinvest"
-              }
-            ].map((project, index) => (
-              <div key={index} className="flex justify-center"> 
-                <CardComponent
-                  title={project.title}
-                  description={project.description}
-                  language={project.language}
-                  imageUrl={project.imageUrl}
-                  role={project.role}
-                  link={project.link}
-                  onMouseEnter={() => handleMouseEnter(index)}
-                  onMouseLeave={handleMouseLeave}
-                  className={hoveredIndex !== null && hoveredIndex !== index ? 'shrink' : ''}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-      </Layout>
     </main>
   );
 }
